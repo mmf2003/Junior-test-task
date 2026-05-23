c6dc5560767c3058e85481047fe43bb7c6592405
Sidelnikov Oleksandr <mmf2003@ukr.netr>
2026-05-23 19:01:13 +0300

    App.js updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import { useRaceSocket } from './hooks/useRaceSocket';
+import RaceBoard from './components/RaceBoard/RaceBoard';
 import './App.css';
 
 function App() {
+  useRaceSocket();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React.
-        </a>
-      </header>
-    </div>
+    <main className="app">
+      <h1>Horse Racing</h1>
+      <RaceBoard />
+    </main>
   );
 }
 

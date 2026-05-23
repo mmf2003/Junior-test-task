aafec9e6ef6c0ed2bc6179f3ddd55f1f4235676f
Sidelnikov Oleksandr <mmf2003@ukr.netr>
2026-05-23 18:51:15 +0300

    index.js updated

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import { store } from './app/store';
+import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>
 );
-

da40080852def38892f6a01ea86187bb3c052415
Sidelnikov Oleksandr <mmf2003@ukr.netr>
2026-05-24 19:59:35 +0300

    Fixed errors from first launch. App  works reliably

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './app/store';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
